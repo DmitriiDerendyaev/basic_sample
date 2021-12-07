5c8c2e3796d0de01f658377a321bbd48bc53e753
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-12-07 19:45:44 +0300

    sample

---
+++ setprecision/setprecision.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double num1 = 3.12345678;
+
+    cout << setprecision(2);
+    cout << num1 << endl;
+
+    return 0;
+}
